acf8ef5193748026419986282df94694e42b4a8c
NirDine <marthindavico@gmail.com>
2023-05-02 02:28:07 -0300

    Progress bars
    Added progress bar tracking.
    
    Academy ruins is set as blue for the purposes of filtering, but adds colorless, I'd have to find a way to track it separately. I'll probably have to add another data attribute, like data-adds or similar.

--- JS/cardHolder.js
+++ JS/cardHolder.js
@@ -1,231 +1,272 @@
-const propertyCheckboxes = document.querySelectorAll('.property'); 
+const propertyCheckboxes = document.querySelectorAll('.property');
 const colorCheckboxes = document.querySelectorAll('.color, .colorless');
 const checkboxes = document.querySelectorAll('.color, .colorless, .property');
 const resetFiltersButton = document.querySelector('.reset-filters');
 const selectedColorCheckboxes = Array.from(colorCheckboxes).filter(checkbox => checkbox.checked);
 const selectedCards = new Set();
 const colorCounts = new Map([
-  ['w', 0],
-  ['u', 0],
-  ['b', 0],
-  ['r', 0],
-  ['g', 0],
-  ['c', 0]
+    ['w', 0],
+    ['u', 0],
+    ['b', 0],
+    ['r', 0],
+    ['g', 0],
+    ['c', 0]
 ]);
 
+
 const properties = new Map([
-  ['ability', 0],
-  ['tap', 0],
-  ['untap', 0],
-  ['artifact', 0],
-  ['creature', 0],
-  ['snow', 0]
+    ['ability', 0],
+    ['tap', 0],
+    ['untap', 0],
+    ['artifact', 0],
+    ['creature', 0],
+    ['snow', 0]
 ]);
 
 const cards = document.querySelectorAll('.card');
 
-const addCard = (card) => {
-  const cardName = card.dataset.cardName;
-  const cardColors = card.dataset.colors.split(',');
 
-  if (selectedCards.has(cardName)) {
-    selectedCards.delete(cardName);
-    card.classList.remove('selected', 'active');
-    document.querySelector(`.addedCard[data-card-name="${cardName}"]`).remove();
-    cardColors.forEach(color => colorCounts.set(color, colorCounts.get(color) - 1));
-  } else {
-    selectedCards.add(cardName);
-    card.classList.add('selected');
-    const addedCard = card.cloneNode(true);
-    addedCard.classList.remove('card');
-    addedCard.classList.add('addedCard');
-    document.querySelector('.chContainer').appendChild(addedCard);
-    cardColors.forEach(color => colorCounts.set(color, colorCounts.get(color) + 1));
-  }
-
-  cards.forEach(card => {
-    const cardColors = card.dataset.colors.split(',');
-    const shouldShowCard = Array.from(selectedColorCheckboxes).every(checkbox => !checkbox.checked || cardColors.includes(checkbox.value));
-    const addedCard = document.querySelector(`.addedCard[data-card-name="${card.dataset.cardName}"]`);
-    card.classList.toggle('hide-card', !shouldShowCard);
-    if (addedCard) addedCard.classList.toggle('hide-card', !shouldShowCard);
+function setTrackerTotals() {
+  const trackerTotals = document.querySelectorAll('[class^="tracker tracker-"] total');
+  trackerTotals.forEach(total => {
+    const recommendedCount = total.dataset.recommended;
+    if (recommendedCount) {
+      total.textContent = recommendedCount;
+    } else {
+     total.style.display = 'none';
+    }
   });
+}
 
-  updateCardsVisibility();
+window.addEventListener('load', setTrackerTotals);
+
+
+const addCard = (card) => {
+    const cardName = card.dataset.cardName;
+    const cardColors = card.dataset.colors.split(',');
+
+    if (selectedCards.has(cardName)) {
+        selectedCards.delete(cardName);
+        card.classList.remove('selected', 'active');
+        document.querySelector(`.addedCard[data-card-name="${cardName}"]`).remove();
+        cardColors.forEach(color => colorCounts.set(color, colorCounts.get(color) - 1));
+    } else {
+        selectedCards.add(cardName);
+        card.classList.add('selected');
+        const addedCard = card.cloneNode(true);
+        addedCard.classList.remove('card');
+        addedCard.classList.add('addedCard');
+        document.querySelector('.chContainer').appendChild(addedCard);
+        cardColors.forEach(color => colorCounts.set(color, colorCounts.get(color) + 1));
+    }
+
+    cards.forEach(card => {
+        const cardColors = card.dataset.colors.split(',');
+        const shouldShowCard = Array.from(selectedColorCheckboxes).every(checkbox => !checkbox.checked || cardColors.includes(checkbox.value));
+        const addedCard = document.querySelector(`.addedCard[data-card-name="${card.dataset.cardName}"]`);
+        card.classList.toggle('hide-card', !shouldShowCard);
+        if (addedCard) addedCard.classList.toggle('hide-card', !shouldShowCard);
+    });
+
+    updateCardsVisibility();
+    updateProgressBar();
+    console.log(colorCounts);
 };
 
 const removeAddedCard = (addedCard) => {
-  const cardName = addedCard.dataset.cardName;
-  selectedCards.delete(cardName);
-  addedCard.remove();
-  addedCard.dataset.colors.split(',').forEach(color => colorCounts.set(color, colorCounts.get(color) - 1));
-  const correspondingCard = document.querySelector(`.card[data-card-name="${cardName}"]`);
-  if (correspondingCard) {
-    correspondingCard.classList.remove('selected');
-  }
+    const cardName = addedCard.dataset.cardName;
+    selectedCards.delete(cardName);
+    addedCard.remove();
+    addedCard.dataset.colors.split(',').forEach(color => colorCounts.set(color, colorCounts.get(color) - 1));
+    const correspondingCard = document.querySelector(`.card[data-card-name="${cardName}"]`);
+    if (correspondingCard) {
+        correspondingCard.classList.remove('selected');
+    }
+    updateProgressBar();
+    console.log(colorCounts);
 };
 
 document.querySelector('.chContainer').addEventListener('click', (event) => {
-  const addedCard = event.target.closest('.addedCard');
-  if (addedCard) removeAddedCard(addedCard);
+    const addedCard = event.target.closest('.addedCard');
+    if (addedCard) removeAddedCard(addedCard);
 });
+
+
+function updateProgressBar() {
+  let totalColorCount = Array.from(colorCounts.values()).reduce((a, b) => a + b, 0);
+
+  const totalCount = Object.values(colorCounts).reduce((acc, count) => acc + count, 0);
+
+  colorCounts.forEach((count, color) => {
+    const percentage = totalColorCount === 0 ? 0 : Math.round(count / totalColorCount * 100);
+    const progressBar = document.querySelector(`.progress-bar-${color}`);
+    progressBar.style.width = `${percentage}%`;
+
+    const currentCount = document.querySelector(`.tracker-${color} current`);
+    currentCount.textContent = count || 0;
+
+    const total = document.querySelector(`.tracker-${color} total`);
+    const recommendedCount = total.dataset.recommended;
+    if (currentCount.textContent >= recommendedCount) {
+      currentCount.classList.add('quotaFilled');
+    } else {
+      currentCount.classList.remove('quotaFilled');
+    }
+  });
+}
+
+
+
 
 
 const updateCardsVisibility = () => {
-  const selectedColors = Array.from(document.querySelectorAll('.color:checked, .colorless:checked')).map(checkbox => checkbox.value);
-  const selectedProperties = Array.from(document.querySelectorAll('.property:checked')).map(checkbox => checkbox.value);
+    const selectedColors = Array.from(document.querySelectorAll('.color:checked, .colorless:checked')).map(checkbox => checkbox.value);
+    const selectedProperties = Array.from(document.querySelectorAll('.property:checked')).map(checkbox => checkbox.value);
 
-  cards.forEach(card => {
-    const cardColors = card.dataset.colors?.split(',') ?? [];
-    const cardProperties = card.dataset.properties?.split(',') ?? [];
+    cards.forEach(card => {
+        const cardColors = card.dataset.colors?.split(',') ?? [];
+        const cardProperties = card.dataset.properties?.split(',') ?? [];
 
-    const shouldShowCard =
-      (selectedColors.length === 0 || cardColors.some(color => selectedColors.includes(color))) &&
-      (selectedProperties.length === 0 || cardProperties.some(prop => selectedProperties.includes(prop)));
+        const shouldShowCard =
+            (selectedColors.length === 0 || cardColors.some(color => selectedColors.includes(color))) &&
+            (selectedProperties.length === 0 || cardProperties.some(prop => selectedProperties.includes(prop)));
 
-    card.classList.toggle('hide-card', !shouldShowCard);
-  });
+        card.classList.toggle('hide-card', !shouldShowCard);
+    });
     updateMobileColorFilters();
 };
 
-    
+
 document.querySelectorAll('.buffet-filters .parent').forEach(parentCheckbox => {
-  parentCheckbox.addEventListener('change', () => {
-    if (parentCheckbox.checked) {
-      document.querySelectorAll('.buffet-filters .parent:checked').forEach(otherChecked => {
-        if (otherChecked !== parentCheckbox) {
-          otherChecked.checked = false;
+    parentCheckbox.addEventListener('change', () => {
+        if (parentCheckbox.checked) {
+            document.querySelectorAll('.buffet-filters .parent:checked').forEach(otherChecked => {
+                if (otherChecked !== parentCheckbox) {
+                    otherChecked.checked = false;
+                }
+            });
         }
-      });
-    }
-  });
+    });
 });
-    
+
 
 resetFiltersButton.addEventListener('click', () => {
-  checkboxes.forEach(checkbox => checkbox.checked = false);
-  cards.forEach(card => card.classList.remove('hide-card'));
-  updateMobileColorFilters();
-    
+    checkboxes.forEach(checkbox => checkbox.checked = false);
+    cards.forEach(card => card.classList.remove('hide-card'));
+    updateMobileColorFilters();
+
 
     $('#search').val('');
     $('#search').trigger('input');
 
 });
-    
+
 checkboxes.forEach(checkbox => checkbox.addEventListener('change', updateCardsVisibility));
 
 cards.forEach(card => {
-  card.addEventListener('click', () => addCard(card));
-  card.addEventListener('keydown', event => (event.code === 'Enter' || event.code === 'Space') && addCard(card));
+    card.addEventListener('click', () => addCard(card));
+    card.addEventListener('keydown', event => (event.code === 'Enter' || event.code === 'Space') && addCard(card));
 });
 
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    const updateMobileColorFilters = () => {
-  const selectedColorCheckboxes = document.querySelectorAll('.color:checked, .colorless:checked');
-  const selectedPropertyCheckboxes = document.querySelectorAll('.section-dropdown .property:checked');
-  
-  const iElements = Array.from(selectedColorCheckboxes).map(cb => {
-    const i = document.createElement('i');
-    i.classList.add('ms', 'ms-' + cb.value);
-    return i;
-  });
-  
-  if (selectedColorCheckboxes.length === 0) {
-    const i = document.createElement('i');
-    i.classList.add('ms', 'ms-multicolor');
-    iElements.push(i);
-  }
-  
-  const targetElements = document.querySelectorAll('.mobileColorTarget');
-  targetElements.forEach(target => {
-    target.querySelectorAll('i').forEach(i => i.remove());
-    iElements.forEach(i => target.appendChild(i));
-  });
-  
-  const propertyElements = document.querySelectorAll('.propertyTarget');
-  propertyElements.forEach(target => {
-    target.querySelectorAll('i').forEach(i => i.remove());
-    Array.from(selectedPropertyCheckboxes).forEach(cb => {
-      const i = document.createElement('i');
-      i.classList.add('ms', 'ms-' + cb.value);
-      target.appendChild(i);
+
+
+const updateMobileColorFilters = () => {
+    const selectedColorCheckboxes = document.querySelectorAll('.color:checked, .colorless:checked');
+    const selectedPropertyCheckboxes = document.querySelectorAll('.section-dropdown .property:checked');
+
+    const iElements = Array.from(selectedColorCheckboxes).map(cb => {
+        const i = document.createElement('i');
+        i.classList.add('ms', 'ms-' + cb.value);
+        return i;
     });
-    if (selectedPropertyCheckboxes.length === 0) {
-      const i = document.createElement('i');
-      i.classList.add('ms', 'ms-land');
-      target.appendChild(i);
+
+    if (selectedColorCheckboxes.length === 0) {
+        const i = document.createElement('i');
+        i.classList.add('ms', 'ms-multicolor');
+        iElements.push(i);
     }
-  });
+
+    const targetElements = document.querySelectorAll('.mobileColorTarget');
+    targetElements.forEach(target => {
+        target.querySelectorAll('i').forEach(i => i.remove());
+        iElements.forEach(i => target.appendChild(i));
+    });
+
+    const propertyElements = document.querySelectorAll('.propertyTarget');
+    propertyElements.forEach(target => {
+        target.querySelectorAll('i').forEach(i => i.remove());
+        Array.from(selectedPropertyCheckboxes).forEach(cb => {
+            const i = document.createElement('i');
+            i.classList.add('ms', 'ms-' + cb.value);
+            target.appendChild(i);
+        });
+        if (selectedPropertyCheckboxes.length === 0) {
+            const i = document.createElement('i');
+            i.classList.add('ms', 'ms-land');
+            target.appendChild(i);
+        }
+    });
 };
 
-    
-  const updateFiltersFromUrl = () => {
-  const queryParams = new URLSearchParams(window.location.search);
-  const selectedColors = queryParams.get('colors')?.split(',') ?? [];
-  const selectedProperties = queryParams.get('properties')?.split(',') ?? [];
 
-  colorCheckboxes.forEach(checkbox => {
-    const value = checkbox.value;
-    checkbox.checked = selectedColors.includes(value);
-  });
+const updateFiltersFromUrl = () => {
+    const queryParams = new URLSearchParams(window.location.search);
+    const selectedColors = queryParams.get('colors')?.split(',') ?? [];
+    const selectedProperties = queryParams.get('properties')?.split(',') ?? [];
 
-  propertyCheckboxes.forEach(checkbox => {
-    const value = checkbox.value;
-    checkbox.checked = selectedProperties.includes(value);
-  });
+    colorCheckboxes.forEach(checkbox => {
+        const value = checkbox.value;
+        checkbox.checked = selectedColors.includes(value);
+    });
+
+    propertyCheckboxes.forEach(checkbox => {
+        const value = checkbox.value;
+        checkbox.checked = selectedProperties.includes(value);
+    });
 };
 
 const updateUrlFromFilters = () => {
-  const selectedColors = Array.from(colorCheckboxes).filter(checkbox => checkbox.checked).map(checkbox => checkbox.value);
-  const selectedProperties = Array.from(propertyCheckboxes).filter(checkbox => checkbox.checked).map(checkbox => checkbox.value);
-  const queryParams = new URLSearchParams();
+    const selectedColors = Array.from(colorCheckboxes).filter(checkbox => checkbox.checked).map(checkbox => checkbox.value);
+    const selectedProperties = Array.from(propertyCheckboxes).filter(checkbox => checkbox.checked).map(checkbox => checkbox.value);
+    const queryParams = new URLSearchParams();
 
-  if (selectedColors.length > 0) {
-    queryParams.set('colors', selectedColors.join(','));
-  }
+    if (selectedColors.length > 0) {
+        queryParams.set('colors', selectedColors.join(','));
+    }
 
-  if (selectedProperties.length > 0) {
-    queryParams.set('properties', selectedProperties.join(','));
-  }
+    if (selectedProperties.length > 0) {
+        queryParams.set('properties', selectedProperties.join(','));
+    }
 
-  const newUrl = `${window.location.protocol}//${window.location.host}${window.location.pathname}?${queryParams.toString()}${window.location.hash}`;
-  window.history.replaceState({ path: newUrl }, '', newUrl);
+    const newUrl = `${window.location.protocol}//${window.location.host}${window.location.pathname}?${queryParams.toString()}${window.location.hash}`;
+    window.history.replaceState({
+        path: newUrl
+    }, '', newUrl);
 };
 
 document.querySelectorAll('.buffet-filters .parent').forEach(parentCheckbox => {
-  parentCheckbox.addEventListener('change', () => {
-    if (parentCheckbox.checked) {
-      document.querySelectorAll('.buffet-filters .parent:checked').forEach(otherChecked => {
-        if (otherChecked !== parentCheckbox) {
-          otherChecked.checked = false;
+    parentCheckbox.addEventListener('change', () => {
+        if (parentCheckbox.checked) {
+            document.querySelectorAll('.buffet-filters .parent:checked').forEach(otherChecked => {
+                if (otherChecked !== parentCheckbox) {
+                    otherChecked.checked = false;
+                }
+            });
         }
-      });
-    }
-  });
+    });
 });
 
 resetFiltersButton.addEventListener('click', () => {
-  checkboxes.forEach(checkbox => checkbox.checked = false);
-  cards.forEach(card => card.classList.remove('hide-card'));
-  updateMobileColorFilters();
-  updateUrlFromFilters();
+    checkboxes.forEach(checkbox => checkbox.checked = false);
+    cards.forEach(card => card.classList.remove('hide-card'));
+    updateMobileColorFilters();
+    updateUrlFromFilters();
 });
 
 checkboxes.forEach(checkbox => checkbox.addEventListener('change', () => {
-  updateCardsVisibility();
-  updateUrlFromFilters();
+    updateCardsVisibility();
+    updateUrlFromFilters();
 }));
 
 
--- JS/colorCombo.js
+++ JS/colorCombo.js
@@ -1,84 +1,84 @@
 $(document).ready(function() {
-  const manaCombinations = {
-    "w": "White",
-    "u": "Blue",
-    "b": "Black",
-    "r": "Red",
-    "g": "Green",
-    "c": "Colorless",
-    "w,u": "Azorius",
-    "u,b": "Dimir",
-    "b,r": "Rakdos",
-    "r,g": "Gruul",
-    "w,g": "Selesnya",
-    "w,b": "Orzhov",
-    "u,r": "Izzet",
-    "b,g": "Golgari",
-    "w,r": "Boros",
-    "u,g": "Simic",
-    "w,u,b": "Esper",
-    "u,b,r": "Grixis",
-    "b,r,g": "Jund",
-    "w,r,g": "Naya",
-    "w,u,g": "Bant",
-    "w,b,g": "Abzan",
-    "w,u,r": "Jeskai",
-    "u,b,g": "Sultai",
-    "w,b,r": "Mardu",
-    "u,r,g": "Temur",
-    "w,u,b,r": "Yore-Tiller",
-    "u,b,r,g": "Glint-Eye",
-    "w,b,r,g": "Dune-Brood",
-    "w,u,r,g": "Ink-Treader",
-    "w,u,b,g": "Witch-Maw",
-    "w,u,b,r,g": "All colors"
-  };
+    const manaCombinations = {
+        "w": "White",
+        "u": "Blue",
+        "b": "Black",
+        "r": "Red",
+        "g": "Green",
+        "c": "Colorless",
+        "w,u": "Azorius",
+        "u,b": "Dimir",
+        "b,r": "Rakdos",
+        "r,g": "Gruul",
+        "w,g": "Selesnya",
+        "w,b": "Orzhov",
+        "u,r": "Izzet",
+        "b,g": "Golgari",
+        "w,r": "Boros",
+        "u,g": "Simic",
+        "w,u,b": "Esper",
+        "u,b,r": "Grixis",
+        "b,r,g": "Jund",
+        "w,r,g": "Naya",
+        "w,u,g": "Bant",
+        "w,b,g": "Abzan",
+        "w,u,r": "Jeskai",
+        "u,b,g": "Sultai",
+        "w,b,r": "Mardu",
+        "u,r,g": "Temur",
+        "w,u,b,r": "Yore-Tiller",
+        "u,b,r,g": "Glint-Eye",
+        "w,b,r,g": "Dune-Brood",
+        "w,u,r,g": "Ink-Treader",
+        "w,u,b,g": "Witch-Maw",
+        "w,u,b,r,g": "All colors"
+    };
 
-  // Cache commonly used DOM elements
-  const $colorless = $('.colorless');
-  const $colors = $('.color');
-  const $combinationName = $('#combination-name');
-  const $explore = $('#explore');
-  let manaList = '';
+    // Cache commonly used DOM elements
+    const $colorless = $('.colorless');
+    const $colors = $('.color');
+    const $combinationName = $('#combination-name');
+    const $explore = $('#explore');
+    let manaList = '';
 
-  // Click event handler for colorless checkbox
-  $colorless.on('click', function() {
-    $colors.prop('checked', false);
-  });
+    // Click event handler for colorless checkbox
+    $colorless.on('click', function() {
+        $colors.prop('checked', false);
+    });
 
-  // Click event handler for color checkboxes
-  $colors.on('click', function() {
-    $colorless.prop('checked', false);
-  });
+    // Click event handler for color checkboxes
+    $colors.on('click', function() {
+        $colorless.prop('checked', false);
+    });
 
-  // Click event handler for mana checkboxes
-  $('.mana input[type=checkbox]').on('click', function() {
-    let $checked = $colors.filter(':checked');
+    // Click event handler for mana checkboxes
+    $('.mana input[type=checkbox]').on('click', function() {
+        let $checked = $colors.filter(':checked');
 
-    // If colorless checkbox is checked, set manaList to 'c'
-    if ($colorless.prop('checked')) {
-      manaList = 'c';
-    } else if ($checked.length) {
-      // Build manaList from checked colored checkboxes
-      manaList = '';
-      $checked.each(function(index) {
-        if (index > 0) {
-          manaList += ',';
+        // If colorless checkbox is checked, set manaList to 'c'
+        if ($colorless.prop('checked')) {
+            manaList = 'c';
+        } else if ($checked.length) {
+            // Build manaList from checked colored checkboxes
+            manaList = '';
+            $checked.each(function(index) {
+                if (index > 0) {
+                    manaList += ',';
+                }
+                manaList += this.value;
+            });
         }
-        manaList += this.value;
-      });
-    }
 
-    // Enable or disable explore button based on whether manaList is empty
-    $explore.prop('disabled', manaList === '');
-      
-    // Set combination name based on manaList
-    $combinationName.html(manaCombinations[manaList]);
-      console.log(manaList);
-  });
+        // Enable or disable explore button based on whether manaList is empty
+        $explore.prop('disabled', manaList === '');
 
-  // Click event handler for explore button
-  $explore.on('click', function() {
-    window.location.href = 'buffet.html?colors=' + manaList;
-  });
+        // Set combination name based on manaList
+        $combinationName.html(manaCombinations[manaList]);
+        console.log(manaList);
+    });
+
+    // Click event handler for explore button
+    $explore.on('click', function() {
+        window.location.href = 'buffet.html?colors=' + manaList;
+    });
 });
